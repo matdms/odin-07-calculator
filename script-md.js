7bfb984b1b9ebeedc5d74d8de9b1e6eb9368e156
matdms <mathieu.desmoulins@gmail.com>
2020-04-11 22:23:36 +0200

    Create function multiply

--- script-md.js
+++ script-md.js
@@ -7,9 +7,15 @@ function add(a, b) {
   else return 'Error';
 }
 
+function multiply(a, b) {
+  if(typeof(a)=='number' && typeof(b)=='number'){
+    return a*b;
+  }
+  else return 'Error';
+}
 
-
-let a = 3;
+let a = 3.2;
 let b = 4;
 
 console.log(a + ' + ' + b + ' = ' + add(a, b));
+console.log(a + ' * ' + b + ' = ' + multiply(a, b));
