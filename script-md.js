5aaf3a8680d5c980ed1ecdcf1954f1fa543f2de2
matdms <mathieu.desmoulins@gmail.com>
2020-04-11 22:34:02 +0200

    Create all 4 operations functions

--- script-md.js
+++ script-md.js
@@ -2,20 +2,41 @@
 
 function add(a, b) {
   if(typeof(a)=='number' && typeof(b)=='number'){
-    return a+b;
+    return Math.round((a+b)*1000)/1000;
   }
-  else return 'Error';
+  //else return 'Error';
+}
+
+function substract(a, b) {
+  if(typeof(a)=='number' && typeof(b)=='number'){
+    return Math.round((a-b)*1000)/1000;
+  }
+  //else return 'Error';
 }
 
 function multiply(a, b) {
   if(typeof(a)=='number' && typeof(b)=='number'){
-    return a*b;
+    return Math.round((a*b)*1000)/1000;
   }
-  else return 'Error';
+  //else return 'Error';
 }
 
-let a = 3.2;
-let b = 4;
+function divide(a, b) {
+  if(typeof(a)=='number' && typeof(b)=='number'){
+    return Math.round((a/b)*1000)/1000;
+  }
+  //else return 'Error';
+}
+
+function operate(operator, a, b) {
+  //takes an operator and 2 numbres and calls the right operation function
+  
+}
+
+let a = 'toto';
+let b = 3;
 
 console.log(a + ' + ' + b + ' = ' + add(a, b));
+console.log(a + ' - ' + b + ' = ' + substract(a, b));
 console.log(a + ' * ' + b + ' = ' + multiply(a, b));
+console.log(a + ' / ' + b + ' = ' + divide(a, b));
