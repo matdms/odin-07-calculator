1dd68bad758643cd4060ad1e0c85fc24b31a8802
matdms <mathieu.desmoulins@gmail.com>
2020-04-11 22:45:07 +0200

    Create function operate

--- script-md.js
+++ script-md.js
@@ -1,5 +1,11 @@
 // Calculator js script
 
+// VARIABLES
+let oper = ['+', '-', '*', '/'];
+let a = 10;
+let b = 3;
+
+// FONCTIONS
 function add(a, b) {
   if(typeof(a)=='number' && typeof(b)=='number'){
     return Math.round((a+b)*1000)/1000;
@@ -30,13 +36,29 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
   //takes an operator and 2 numbres and calls the right operation function
-  
+  if(oper.includes(operator) && typeof(a)=='number' && typeof(b)=='number') {
+    switch(operator) {
+      case '+':
+        return add(a, b);
+      case '-':
+        return substract(a, b);
+      case '*':
+        return multiply(a, b);
+      case '/':
+        return divide(a, b);
+    }
+  }
 }
 
-let a = 'toto';
-let b = 3;
 
-console.log(a + ' + ' + b + ' = ' + add(a, b));
+// MAIN
+
+/*console.log(a + ' + ' + b + ' = ' + add(a, b));
 console.log(a + ' - ' + b + ' = ' + substract(a, b));
 console.log(a + ' * ' + b + ' = ' + multiply(a, b));
-console.log(a + ' / ' + b + ' = ' + divide(a, b));
+console.log(a + ' / ' + b + ' = ' + divide(a, b));*/
+
+console.log(a + ' + ' + b + ' = ' + operate('+', a, b));
+console.log(a + ' - ' + b + ' = ' + operate('-', a, b));
+console.log(a + ' * ' + b + ' = ' + operate('*', a, b));
+console.log(a + ' / ' + b + ' = ' + operate('/', a, b));
